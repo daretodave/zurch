84c6989881497d0d2edbf6554b7acf9563608326
David Rehmat <me@dave.blue>
2018-12-29 19:06:58 -0500

    release target

--- bin/upgrade.js
+++ bin/upgrade.js
@@ -1,78 +1,64 @@
 const path = require('path');
+const fs = require('fs');
 
 const semver = require('semver');
 const inquirer = require('inquirer');
 const Git = require("nodegit");
 
 const package = require("../package");
-
 const version = package.version;
-const release = {
-    major: semver.inc(version, "major"),
-    minor: semver.inc(version, "minor"),
-    patch: semver.inc(version, "patch")
-};
+
 inquirer.prompt([
     {
         type: 'list',
         name: 'channel',
-        message: [package.name+"@"+version + " | release type: "],
-        filter: function(val) {
+        message: [package.name + "@" + version + " | release type: "],
+        filter: function (val) {
             return val.toLowerCase();
         },
         choices: [
             'patch',
             'minor',
-            'major'
+            'major',
+            'none'
         ]
-    }
+    },
+    {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Ready to go?',
+        default: true
+    },
 ]).then(answers => {
-    console.log(JSON.stringify(answers, null, '  '));
+    const {channel, confirm} = answers;
+    if (!confirm) {
+        return;
+    }
+    if (channel === "none") {
+        console.log("publish | done, version already in manifest");
+        return;
+    }
+
+    const pending = semver.inc(version, channel);
+    const app = Object.assign({}, package, {
+        version: pending
+    });
+    const json = JSON.stringify(app, null, 2);
+    const file = path.join(__dirname, '..', 'package.json');
+
+    console.log("publish | setting version to " + pending);
+    console.log("publish | package.json = " + json);
+    console.log("publish | path = " + file);
+
+    fs.writeFileSync(file, json, "utf8");
+
+    console.log("publish | done")
 });
 
-const pending = semver.inc(version, "major");
-
-console.log(pending);
-
-Git.Repository
-    .open(path.join(__dirname, '..'))
-// Open the master branch.
-    .then(function(repo) {
-        return repo.getTagByName("1.0.10");
-    })
-    // Display information about commits on master.
-    .then(function(firstCommitOnMaster) {
-        // Create a new history event emitter.
-        var history = firstCommitOnMaster.history();
-
-        // Create a counter to only show up to 9 entries.
-        var count = 0;
-
-        // Listen for commit events from the history.
-        history.on("commit", function(commit) {
-            // Disregard commits past 9.
-            if (++count >= 9) {
-                return;
-            }
-
-            // Show the commit sha.
-            console.log("commit " + commit.sha());
-
-            // Store the author object.
-            var author = commit.author();
-
-            // Display author information.
-            console.log("Author:\t" + author.name() + " <" + author.email() + ">");
-
-            // Show the commit date.
-            console.log("Date:\t" + commit.date());
-
-            // Give some space and show the message.
-            console.log("\n    " + commit.message());
-        });
-
-        // Start emitting events.
-        history.start();
-    }).catch(e => {
-        console.error(e)
-})
+async function notes() {
+    const repo = await Git.Repository.open(path.join(__dirname, '..'));
+    const tags = await Git.Tag.list(repo);
+    const noteList = await Promise.all(tags.map(o => repo.getReference(`refs/tags/${o}`)));
+    const targets = await Promise.all(noteList.map(o => repo.getTag(o.target())));
+    return targets.map(o => o.message());
+}
