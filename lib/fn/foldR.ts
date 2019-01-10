a0d272dc7b16c158f20333d316913db82ec8b80e
unknown <no.trbl.2.u@gmail.com>
2019-01-10 18:04:56 -0500

    Created branch "foldR" ++ created comments to guide definition

---
+++ lib/fn/foldR.ts
@@ -0,0 +1,13 @@
+
+//           Fold Right                //
+// Haskell's foldR w/ pattern matching //
+
+/* 
+  fr :: (a -> b -> b) -> b -> [a] -> b
+  fr f v []     = v
+  fr f v (x:xs) = f x (fr f v xs)
+*/
+
+// PATTERN TO BUILD WITH FOLDR  || USAGE:
+// f []     = v                 || v = Identity/Base-Case
+// f (x:xs) = x (+) f xs        || func(head xs) f(xs)
