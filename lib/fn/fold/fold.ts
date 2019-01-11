b058757ed8681aa760d04752afaf716722891b07
unknown <no.trbl.2.u@gmail.com>
2019-01-11 18:51:43 -0500

    Continued Fold, added Jest --watch script

--- lib/fn/fold/fold.ts
+++ lib/fn/fold/fold.ts
@@ -13,7 +13,27 @@ import { log } from '../../../../shapes-n-lances/lib/common/index';
 // f []     = v                 || v = Identity/Base-Case
 // f (x:xs) = x (+) f xs        || func(head xs) f(xs)
 
-export function fold () {
-  log("In foldR");
-  return 0
+export const 
+  fold = func => base => collection => {
+    const head = arr => arr[0]
+    let accum = []
+    
+    // if ([]) {
+      // return base;
+    // }else{
+      // return fold(func)(collection[0])(collection.slice(0, collection.length - 1));
+      // return fold(func(accum.concat(func(head(collection)))))
+      // return fold(func)(base)(func(accum) += (tail(collection)))
+    // }
+    return
 }
+
+/*
+  Take:
+    -func
+    -base
+    -collection
+  
+  1. Take the head of the collection
+  2. Apply the function to it
+*/
--- lib/fn/fold/foldR.test.ts
+++ lib/fn/fold/foldR.test.ts
@@ -1,7 +1,28 @@
 import { fold } from './fold';
 
+//           Fold Right                //
+// Haskell's foldR w/ pattern matching //
+
+/* 
+  fr :: (a -> b -> b) -> b -> [a] -> b
+  fr f v []     = v
+  fr f v (x:xs) = f x (fr f v xs)
+*/
+
+// PATTERN TO BUILD WITH FOLDR  || USAGE:
+// f []     = v                 || v = Identity/Base-Case
+// f (x:xs) = x (+) f xs        || func(head xs) f(xs)
+
 describe('fold', () => {
-  test('Returns 0', () => {
-    expect(fold()).toBe(0);
-  });
+
+  test('Should fold list', () => {
+    let numbers = [1, 2, 3]
+    let add = n => m => n + m
+
+    let sum = fold(add)(0)
+
+    expect(sum(numbers)).toEqual(6)
+  })
+
 });
+
