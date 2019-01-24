27ead2354c05ec48409134bcc54d51c322063e0a
unknown <no.trbl.2.u@gmail.com>
2019-01-24 03:37:28 -0500

    Began creating better compose

---
+++ lib/fn/compose/compose.ts
@@ -0,0 +1,12 @@
+import { curry } from '../curry/curry';
+
+// x <- fn1(x) <- f2(x)
+export const compose = fn1 => fn2 => x => fn1(fn2(x));
+export const
+compose_ = (...fns) => {
+  const [fn1, fn2, ...rest] = fns.reverse();
+  const composedFN = (...args) => fn2(fn1(...args));
+  return !(rest.length)
+    ? composedFN 
+    : compose_(...rest.reverse(), composedFN) 
+}
